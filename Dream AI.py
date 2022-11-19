9e7b37de56c949026de937cef9809d09325ba4c0
Himanthroxx <118628357+Himanthroxx@users.noreply.github.com>
2022-11-19 23:35:55 +0530

    Add files via upload

---
+++ Dream AI.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a = [10,11,50,34]
+b = []
+for i in range(len(a)):
+    x = int(str(a[i])+str(i))
+    b.append(x)
+print(b)
+
+
+# In[ ]:
+
+
+
+
